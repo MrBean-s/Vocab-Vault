d8721240430b2f483c5fc9437799379fcb0185ae
Brandon <bravellaneda21@gmail.com>
2026-06-20 00:07:12 -0600

    Language crud

--- dictApp/forms.py
+++ dictApp/forms.py
@@ -2,11 +2,18 @@ from django import forms
 from .models import Language, Image
 
 class LanguageForm(forms.ModelForm):
-	image_file = forms.ImageField()
+	image_file = forms.ImageField(
+		required=True,
+        widget=forms.FileInput(attrs={'class': 'form-control'})
+    )
 
 	class Meta:
 		model = Language
 		fields = ['name']
+		widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'image_file': forms.FileInput(attrs={'class': 'form-control'})
+        }
 
 
 	def save(self, commit=True):
@@ -14,11 +21,15 @@ class LanguageForm(forms.ModelForm):
 		image_file = self.cleaned_data.get('image_file')
 
 		if image_file:
-			if language.pk and language.image:
-				language.image.delete()
+			old_img = language.image
+			if language.pk and old_img:
+				storage = old_img.file.storage
+				if storage.exists(old_img.file.name):
+					storage.delete(old_img.file.name)
+				old_img.delete()
 
-			img = Image.objects.create(file=image_file)
-			language.image = img
+			new_img = Image.objects.create(file=image_file)
+			language.image = new_img
 
 		language.save() #can't use if commmit = False cause that'd leave an orphan img
 
--- dictApp/urls.py
+++ dictApp/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
 	path("", views.start_screen, name="start_screen"),
 	path("lang/<int:lang_id>/dashboard/", views.dashboard, name="dashboard"),
-	path("lang/add/", views.language_form, name="add_lang"),
-	path("lang/<int:lang_id>/edit/", views.language_form, name="add_lang"),
+	path("lang/form", views.language_form, name="lang_modal_form"),
+	path("lang/<int:lang_id>/edit/", views.language_form, name="lang_edit_form"),
+	path("lang/<int:lang_id>/delete/", views.delete_language, name="lang_delete"),
+	path("languages", views.languages, name="languages"),
+	path("generic/confirm-delete/", views.generic_confirm_delete, name="generic_confirm_delete"),
 ]
