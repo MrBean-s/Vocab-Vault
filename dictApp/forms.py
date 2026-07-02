4bc71dfe4f526a31d073ad975ca6d5090cd3a250
Brandon <bravellaneda21@gmail.com>
2026-07-01 19:02:44 -0600

    identation hack job + seach modal working

--- dictApp/forms.py
+++ dictApp/forms.py
@@ -4,137 +4,184 @@ from django.forms import inlineformset_factory
 from django.forms.models import BaseInlineFormSet
 
 class LanguageForm(forms.ModelForm):
-    image_file = forms.ImageField(
-        required=True,
-        widget=forms.FileInput(attrs={'class': 'form-control'})
-    )
+   image_file = forms.ImageField(
+      required=True,
+      widget=forms.FileInput(attrs={'class': 'form-control'})
+   )
 
-    class Meta:
-        model = Language
-        fields = ['name']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'image_file': forms.FileInput(attrs={'class': 'form-control'})
-        }
+   class Meta:
+      model = Language
+      fields = ['name']
+      widgets = {
+         'name': forms.TextInput(attrs={'class': 'form-control'}),
+         'image_file': forms.FileInput(attrs={'class': 'form-control'})
+      }
 
 
-    def save(self, commit=True):
-        language = super().save(commit=False)
-        image_file = self.cleaned_data.get('image_file')
+   def save(self, commit=True):
+      language = super().save(commit=False)
+      image_file = self.cleaned_data.get('image_file')
 
-        if image_file:
-            old_img = language.image
-            if language.pk and old_img:
-                storage = old_img.file.storage
-                if storage.exists(old_img.file.name):
-                    storage.delete(old_img.file.name)
-                old_img.delete()
+      if image_file:
+         old_img = language.image
+      if language.pk and old_img:
+         storage = old_img.file.storage
+         if storage.exists(old_img.file.name):
+            storage.delete(old_img.file.name)
+         old_img.delete()
 
-            new_img = Image.objects.create(file=image_file)
-            language.image = new_img
+      new_img = Image.objects.create(file=image_file)
+      language.image = new_img
 
-        language.save() #can't use if commmit = False cause that'd leave an orphan img
+      language.save() #can't use if commmit = False cause that'd leave an orphan img
 
-        return language
+      return language
 
 class WordForm(forms.ModelForm):
-    class Meta:
-        model = Word
-        fields = ['name']
-        labels = { 'name': 'Word or phrase name'}
-        widgets = {
-            'name': forms.TextInput(attrs={
-                    'class': 'form-control banner-input field-important underline-important',
-                    'placeholder': 'Word name'
-                }),
-        }
+   class Meta:
+      model = Word
+      fields = ['name']
+      labels = { 'name': 'Word or phrase name'}
+      widgets = {
+         'name': forms.TextInput(attrs={
+            'class': 'form-control banner-input field-important underline-important',
+            'placeholder': 'Word name'
+         }),
+      }
 
 class DefinitionForm(forms.ModelForm):
-    image_file = forms.ImageField(
-        required=False,
-        widget=forms.FileInput(attrs={'class': 'form-control img-input',})
-    )
+   image_file = forms.ImageField(
+      required=False,
+      widget=forms.FileInput(attrs={'class': 'form-control img-input',})
+   )
 
-    class Meta:
-        model = Definition
-        fields = ['description', 'origin']
-        labels = {
-            'description': 'Defintion',
-            'origin': 'Definition origin'
-        }
-        widgets = {
-            'description': forms.Textarea(
-                attrs={
-                    'rows': 3, 
-                    'cols': 50, 
-                    'class': 'form-control resize-none field-description',
-                    'manual-required': 'true'
-                }
-            ),
-            'origin': forms.Textarea(
-                attrs={
-                    'rows': 2, 
-                    'cols': 50, 
-                    'class': 'form-control resize-none field-description',
-                }
-            ),
-            'image_file': forms.FileInput(
-                attrs={
-                    'class': 'form-control p-5'
-                }
-            )
-        }
+   class Meta:
+      model = Definition
+      fields = ['description', 'origin']
+      labels = {
+         'description': 'Defintion',
+         'origin': 'Definition origin'
+      }
+      widgets = {
+         'description': forms.Textarea(
+            attrs={
+               'rows': 3, 
+               'cols': 50, 
+               'class': 'form-control resize-none field-description',
+               'manual-required': 'true'
+            }
+         ),
+         'origin': forms.Textarea(
+            attrs={
+               'rows': 2, 
+               'cols': 50, 
+               'class': 'form-control resize-none field-description',
+            }
+         ),
+         'image_file': forms.FileInput(
+            attrs={
+               'class': 'form-control p-5'
+            }
+         )
+      }
 
-    
+
 
 class ExampleForm(forms.ModelForm):
-    class Meta:
-        model = Example
-        fields = ['description', 'explanation', 'custom_audio', 'part_of_speech']
-        labels = {
-            'description': 'Example ',
-            'explanation': 'In other words / Explanation'
-        }
-        widgets = {
-            'description': forms.Textarea(
-                attrs={
-                    'rows': 2,
-                    'cols': 50, 
-                    'class': 'form-control resize-none field-quote',
-                    'placeholder': 'Write an example',
-                    'manual-required': 'true'
-                }
-            ),
-            'explanation': forms.Textarea(
-                attrs={
-                    'rows': 2, 
-                    'cols': 50, 
-                    'class': 'form-control resize-none field-explanation',
-                    'placeholder': 'Explained in other words'
-                }
-            ),
-        }
+   class Meta:
+      model = Example
+      fields = ['description', 'explanation', 'custom_audio', 'part_of_speech']
+      labels = {
+      'description': 'Example ',
+      'explanation': 'In other words / Explanation'
+      }
+      widgets = {
+         'description': forms.Textarea(
+            attrs={
+               'rows': 2,
+               'cols': 50, 
+               'class': 'form-control resize-none field-quote',
+               'placeholder': 'Write an example',
+               'manual-required': 'true'
+            }
+         ),
+         'explanation': forms.Textarea(
+            attrs={
+               'rows': 2, 
+               'cols': 50, 
+               'class': 'form-control resize-none field-explanation',
+               'placeholder': 'Explained in other words'
+            }
+         ),
+      }
 
 class SkipEmptyDeletedInlineFormSet(BaseInlineFormSet):
-    def clean(self):
-        super().clean()
-        for form in self.forms:
-            if self._should_delete_form(form):
-                # Discard all field errors – this row is going to be deleted
-                form._errors = {}
+   def clean(self):
+      super().clean()
+      for form in self.forms:
+         if self._should_delete_form(form):
+            # Discard all field errors – this row is going to be deleted
+            form._errors = {}
 
 DefinitionFormSet = inlineformset_factory(
-    Word, Definition,
-    form=DefinitionForm,
-    formset=SkipEmptyDeletedInlineFormSet,
-    extra=1,
-    can_delete=True
+   Word, Definition,
+   form=DefinitionForm,
+   formset=SkipEmptyDeletedInlineFormSet,
+   extra=1,
+   can_delete=True
 )
 
 ExampleFormSet = inlineformset_factory(
-    Definition, Example,
-    form=ExampleForm,
-    formset=SkipEmptyDeletedInlineFormSet,
-    extra=1,
-    can_delete=True
+   Definition, Example,
+   form=ExampleForm,
+   formset=SkipEmptyDeletedInlineFormSet,
+   extra=1,
+   can_delete=True
 )
+
+class ModalSearchForm(forms.Form):
+
+   SEARCH_FILTER = [
+      ('word', 'Words'),
+      ('definition', 'Definitions'),
+      ('example', 'Examples'),
+   ]
+
+   query = forms.CharField(
+      max_length=100,
+      required=True,
+      widget=forms.TextInput(attrs={
+         'class': 'form-control',
+         'placeholder': 'Search...'
+      })
+   )
+
+   filter = forms.MultipleChoiceField(
+      choices=SEARCH_FILTER,
+      widget=forms.CheckboxSelectMultiple(attrs={}),
+      required=False,
+   )
+
+   limit = forms.IntegerField(
+      min_value=1,
+      max_value=30,
+      initial=5,
+      required=False,
+      widget=forms.NumberInput(attrs={'class': 'form-control'})
+   )
+
+   newest_first = forms.BooleanField(
+      required=False,
+      initial=True,
+      widget=forms.CheckboxInput(attrs={'class': 'form-check-input', 'role': 'switch'})
+   )
+
+   def clean(self):
+      cleaned_data = super().clean()
+
+      filter = cleaned_data.get('filter')
+      if not filter:
+         raise forms.ValidationError("At least one filter is required.")
+
+      return cleaned_data
+
--- dictApp/urls.py
+++ dictApp/urls.py
@@ -3,15 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.start_screen, name="start_screen"),
-	path("lang/<int:lang_id>/dashboard/", views.dashboard, name="dashboard"),
-	path("lang/form", views.language_form, name="lang_modal_form"),
-	path("lang/<int:lang_id>/edit/", views.language_form, name="lang_edit_form"),
-	path("lang/<int:lang_id>/delete/", views.delete_language, name="lang_delete"),
-	path("languages", views.languages, name="languages"),
-	path("generic/confirm-delete/", views.generic_confirm_delete, name="generic_confirm_delete"),
-	path("lang/<int:lang_id>/word/", views.word, name="word"),
-	path("lang/<int:lang_id>/word/<int:word_id>", views.word, name="word_edit"),
-	path("lang/<int:lang_id>/words", views.word_list, name="word_list"),
-  path("lang/<int:lang_id>/import/", views.import_words_from_old_json, name="import"),
+   path("", views.start_screen, name="start_screen"),
+   path("lang/<int:lang_id>/dashboard/", views.dashboard, name="dashboard"),
+   path("lang/form", views.language_form, name="lang_modal_form"),
+   path("lang/<int:lang_id>/edit/", views.language_form, name="lang_edit_form"),
+   path("lang/<int:lang_id>/delete/", views.delete_language, name="lang_delete"),
+   path("languages", views.languages, name="languages"),
+   path("generic/confirm-delete/", views.generic_confirm_delete, name="generic_confirm_delete"),
+   path("lang/<int:lang_id>/word/", views.word, name="word"),
+   path("lang/<int:lang_id>/word/<int:word_id>", views.word, name="word_edit"),
+   path("lang/<int:lang_id>/words/", views.word_list, name="word_list"),
+   path("lang/<int:lang_id>/import/", views.import_words_from_old_json, name="import"),
+   path("lang/<int:lang_id>/search/", views.search, name="search")
 ]
